a8b16dfc68f4a2b3ddff9c35b9bf8e8064cb56f8
Maria <r.m.u@bk.ru>
2023-01-31 15:38:26 +0300

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Домашнее задание по 1 семинару.
+// Задача 1.
+// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Считаем, что пользователь может вводить только числа, целые или дробные.
+
+
+Console.WriteLine("Введите число a:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else if (a < b)
+{
+    Console.WriteLine("max = " + b);
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
